4473086de28ab953d1207a2eb88f9bf77875b91b
BillyZhangGuoping <18616202291@qq.com>
2020-06-11 17:46:51 +0800

    Complete CTA backtesting of Batch, add portfolio result

--- VNPY2_BILLY/Batch_CTABacktesting/BatchCTABacktesting.py
+++ VNPY2_BILLY/Batch_CTABacktesting/BatchCTABacktesting.py
@@ -2,10 +2,11 @@
 import json
 import traceback
 from datetime import datetime, date
-
+import pandas as pd
 from pandas import DataFrame
 from vnpy.app.cta_strategy.backtesting import BacktestingEngine
-from vnpy.app.cta_strategy.strategies.atr_rsi_strategy import AtrRsiStrategy
+from vnpy.app.cta_strategy.strategies.boll_channel_strategy import BollChannelStrategy
+from vnpy.app.cta_strategy.strategies.turtle_signal_strategy import TurtleSignalStrategy
 from vnpy.app.cta_strategy.strategies.double_ma_strategy import DoubleMaStrategy
 
 class BatchCTABackTest:
@@ -19,15 +20,14 @@ class BatchCTABackTest:
 		"""
 		config = open(vtSymbolconfig)
 		self.setting = json.load(config)
-		self.engine = BacktestingEngine()
 		self.exportpath = exportpath
 
-	def addParameters(self, vt_symbol: str, startDate, endDate, interval="1m", capital=1_000_000):
+	def addParameters(self, engine, vt_symbol: str, startDate, endDate, interval="1m", capital=1_000_000):
 		"""
 		从vtSymbol.json文档读取品种的交易属性，比如费率，交易每跳，比率，滑点
 		"""
 		if vt_symbol in self.setting:
-			self.engine.set_parameters(
+			engine.set_parameters(
 				vt_symbol=vt_symbol,
 				interval=interval,
 				start=startDate,
@@ -40,61 +40,71 @@ class BatchCTABackTest:
 			)
 		else:
 			print("symbol %s hasn't be maintained in config file" % vt_symbol)
-		return None
+		return engine
 
-	def runBatchTest(self, strategy_setting, startDate, endDate):
+	def runBatchTest(self, strategy_setting, startDate, endDate, portfolio):
 		"""
 		进行回测
 		"""
 		resultDf = DataFrame()
+		dfportfolio = None
 		for strategy_name, strategy_config in strategy_setting.items():
+			engine = BacktestingEngine()
 			vt_symbol = strategy_config["vt_symbol"]
-			self.addParameters(vt_symbol, startDate, endDate)
-			self.engine.add_strategy(
-				eval(strategy_config["class_name"]),
-				strategy_config["setting"]
-			)
-			self.engine.load_data()
-			self.engine.run_backtesting()
-			df = self.engine.calculate_result()
-			resultDict = self.engine.calculate_statistics(df, False)
+			engine = self.addParameters(engine, vt_symbol, startDate, endDate)
+			if type(strategy_config["setting"]) is str:
+				print(strategy_config["setting"])
+				engine.add_strategy(
+					eval(strategy_config["class_name"]),
+					json.loads(strategy_config["setting"], )
+				)
+			else:
+				engine.add_strategy(
+					eval(strategy_config["class_name"]),
+					strategy_config["setting"]
+				)
+			engine.load_data()
+			engine.run_backtesting()
+			df = engine.calculate_result()
+			if portfolio == True:
+				if dfportfolio is None:
+					dfportfolio = df
+				else:
+					dfportfolio = dfportfolio + df
+			resultDict = engine.calculate_statistics(df, False)
 			resultDict["class_name"] = strategy_config["class_name"]
 			resultDict["setting"] = strategy_config["setting"]
 			resultDict["vt_symbol"] = strategy_config["vt_symbol"]
 			resultDf = resultDf.append(resultDict, ignore_index=True)
+
+		if portfolio == True:
+			# dfportfolio = dfportfolio.dropna()
+			engine = BacktestingEngine()
+			engine.calculate_statistics(dfportfolio)
+			engine.show_chart(dfportfolio)
 		return resultDf
 
-	def runBatchTestJson(self, jsonpath="ctaStrategy.json", startDate=datetime(2019, 1, 1),
-	                     endDate=datetime(2020, 1, 1), exporpath=None):
+	def runBatchTestJson(self, jsonpath="ctaStrategy.json", startDate=datetime(2019, 7, 1),
+	                     endDate=datetime(2020, 1, 1), exporpath=None, portfolio=True):
 		"""
 		从ctaStrategy.json去读交易策略和参数，进行回测
 		"""
 		with open(jsonpath, mode="r", encoding="UTF-8") as f:
 			strategy_setting = json.load(f)
-		resultDf = self.runBatchTest(strategy_setting, startDate, endDate)
-		# for strategy_name, strategy_config in strategy_setting.items():
-		# 	vt_symbol = strategy_config["vt_symbol"]
-		# 	self.addParameters(vt_symbol, startDate, endDate)
-		# 	self.engine.add_strategy(
-		# 		eval(strategy_config["class_name"]),
-		# 		strategy_config["setting"]
-		# 	)
-		# 	self.engine.load_data()
-		# 	self.engine.run_backtesting()
-		# 	df = self.engine.calculate_result()
-		# 	resultDict = self.engine.calculate_statistics(df, False)
-		# 	resultDict["class_name"] = strategy_config["class_name"]
-		# 	resultDict["setting"] = strategy_config["setting"]
-		# 	resultDict["vt_symbol"]= strategy_config["vt_symbol"]
-		# 	self.resultDf = self.resultDf.append(resultDict, ignore_index=True)
+		resultDf = self.runBatchTest(strategy_setting, startDate, endDate, portfolio)
 		self.ResultExcel(resultDf, exporpath)
-		return resultDf
+		return strategy_setting
 
-	def runBatchTestCSV(self, csvpath, expeort='Excel'):
+	def runBatchTestExcecl(self, path="ctaStrategy.xls", startDate=datetime(2019, 7, 1),
+	                       endDate=datetime(2020, 1, 1), exporpath=None, portfolio=False):
 		"""
 		从ctaStrategy.excel去读交易策略和参数，进行回测
 		"""
-		return None
+		df = pd.read_excel(path)
+		strategy_setting = df.to_dict(orient='index')
+		resultDf = self.runBatchTest(strategy_setting, startDate, endDate, portfolio)
+		self.ResultExcel(resultDf, exporpath)
+		return strategy_setting
 
 	def ResultExcel(self, result, export=None):
 		"""
@@ -108,7 +118,6 @@ class BatchCTABackTest:
 			path = exportpath + "CTABatch" + str(date.today()) + "v0.xls"
 			result.to_excel(path, index=False)
 			print("CTA Batch result is export to %s" % path)
-
 		except:
 			print(traceback.format_exc())
 
